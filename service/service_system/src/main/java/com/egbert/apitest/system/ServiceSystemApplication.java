0ed9285fd24d9685bd14f8b54d84a969225459a0
EgbertHistorianPokling <moxiaoxuanyu@foxmail.com>
2021-05-05 19:26:00 +0800

    系统模块后端基本完善

--- service/service_system/src/main/java/com/egbert/apitest/system/ServiceSystemApplication.java
+++ service/service_system/src/main/java/com/egbert/apitest/system/ServiceSystemApplication.java
@@ -2,8 +2,10 @@ package com.egbert.apitest.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.egbert")
 public class ServiceSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceSystemApplication.class, args);
--- service/service_system/src/main/java/com/egbert/apitest/system/controller/SystemSetController.java
+++ service/service_system/src/main/java/com/egbert/apitest/system/controller/SystemSetController.java
@@ -1,10 +1,12 @@
 package com.egbert.apitest.system.controller;
 
+import com.egbert.apitest.common.result.Result;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.egbert.apitest.model.sys.SystemSet;
 import com.egbert.apitest.system.service.SystemSetService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
@@ -13,8 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Random;
 
+@Api(tags="系统设置管理")
 @RestController
-@RequestMapping("/sys/systemSet")
+@RequestMapping("/api/sys/systemSet")
 public class SystemSetController {
 
     //注入service
@@ -23,28 +26,20 @@ public class SystemSetController {
 
     // http://localhost:8201/sys/systemSet/findAll
 
-    @GetMapping("findAll")
-    public List<SystemSet> findAllSystemSet() {
-        //调用service的方法
-        List<SystemSet> list = systemSetService.list();
-        return list;
-    }
-
-    /*
     //1 查询系统设置表所有信息
     @ApiOperation(value = "获取所有系统设置")
     @GetMapping("findAll")
-    public Result findAllHospitalSet() {
+    public Result findAllSystemSet() {
         //调用service的方法
-        List<HospitalSet> list = hospitalSetService.list();
+        List<SystemSet> list = systemSetService.list();
         return Result.ok(list);
     }
 
     //2 逻辑删除医院设置
-    @ApiOperation(value = "逻辑删除医院设置")
-    @DeleteMapping("{id}")
-    public Result removeHospSet(@PathVariable Long id) {
-        boolean flag = hospitalSetService.removeById(id);
+    @ApiOperation(value = "逻辑删除系统设置")
+    @DeleteMapping("delete/{id}")
+    public Result removeSystemSet(@PathVariable Long id) {
+        boolean flag = systemSetService.removeById(id);
         if(flag) {
             return Result.ok();
         } else {
@@ -53,40 +48,39 @@ public class SystemSetController {
     }
 
     //3 条件查询带分页
-    @PostMapping("findPageHospSet/{current}/{limit}")
-    public Result findPageHospSet(@PathVariable long current,
+    @ApiOperation(value = "条件查询带分页")
+    @PostMapping("findPageSystemSet/{current}/{limit}")
+    public Result findPageSystemSet(@PathVariable long current,
                                   @PathVariable long limit,
-                                  @RequestBody(required = false) com.atguigu.yygh.vo.hosp.HospitalSetQueryVo hospitalSetQueryVo) {
+                                  @RequestBody(required = false) com.egbert.apitest.vo.sys.SystemSetQueryVo systemSetQueryVo) {
         //创建page对象，传递当前页，每页记录数
-        Page<HospitalSet> page = new Page<>(current,limit);
+        Page<SystemSet> page = new Page<>(current,limit);
         //构建条件
-        QueryWrapper<HospitalSet> wrapper = new QueryWrapper<>();
-        String hosname = hospitalSetQueryVo.getHosname();//医院名称
-        String hoscode = hospitalSetQueryVo.getHoscode();//医院编号
-        if(!StringUtils.isEmpty(hosname)) {
-            wrapper.like("hosname",hospitalSetQueryVo.getHosname());
+        QueryWrapper<SystemSet> wrapper = new QueryWrapper<>();
+        String sysname = systemSetQueryVo.getSysname();//x系统名称
+        String sysdescription = systemSetQueryVo.getSysdescription();//系统编号
+        if(!StringUtils.isEmpty(sysname)) {
+            wrapper.like("sysname",systemSetQueryVo.getSysname());
         }
-        if(!StringUtils.isEmpty(hoscode)) {
-            wrapper.eq("hoscode",hospitalSetQueryVo.getHoscode());
+        if(!StringUtils.isEmpty(sysdescription)) {
+            wrapper.like("description",systemSetQueryVo.getSysdescription());
         }
 
         //调用方法实现分页查询
-        IPage<HospitalSet> pageHospitalSet = hospitalSetService.page(page, wrapper);
+        IPage<SystemSet> pageSystemSet = systemSetService.page(page, wrapper);
 
         //返回结果
-        return Result.ok(pageHospitalSet);
+        return Result.ok(pageSystemSet);
     }
 
-    //4 添加医院设置
-    @PostMapping("saveHospitalSet")
-    public Result saveHospitalSet(@RequestBody HospitalSet hospitalSet) {
+    //4 添加系统设置
+    @ApiOperation(value = "添加系统设置")
+    @PostMapping("addSystemSet")
+    public Result saveSystemSet(@RequestBody SystemSet systemSet) {
         //设置状态 1 使用 0 不能使用
-        hospitalSet.setStatus(1);
-        //签名秘钥
-        Random random = new Random();
-        hospitalSet.setSignKey(MD5.encrypt(System.currentTimeMillis()+""+random.nextInt(1000)));
+        systemSet.setStatus(1);
         //调用service
-        boolean save = hospitalSetService.save(hospitalSet);
+        boolean save = systemSetService.save(systemSet);
         if(save) {
             return Result.ok();
         } else {
@@ -94,24 +88,26 @@ public class SystemSetController {
         }
     }
 
-    //5 根据id获取医院设置
-    @GetMapping("getHospSet/{id}")
-    public Result getHospSet(@PathVariable Long id) {
+    //5 根据id获取系统设置
+    @ApiOperation(value = "根据id获取系统设置")
+    @GetMapping("getSysSet/{id}")
+    public Result getSysSet(@PathVariable Long id) {
 //        try {
 //            //模拟异常
 //            int a = 1/0;
 //        }catch (Exception e) {
-//            throw new YyghException("失败",201);
+//            throw new ApitestException("失败",201);
 //        }
 
-        HospitalSet hospitalSet = hospitalSetService.getById(id);
-        return Result.ok(hospitalSet);
+        SystemSet systemSet = systemSetService.getById(id);
+        return Result.ok(systemSet);
     }
 
-    //6 修改医院设置
-    @PostMapping("updateHospitalSet")
-    public Result updateHospitalSet(@RequestBody HospitalSet hospitalSet) {
-        boolean flag = hospitalSetService.updateById(hospitalSet);
+    //6 修改系统设置
+    @ApiOperation(value = "修改系统设置")
+    @PostMapping("updateSystemSet")
+    public Result updateSystemSet(@RequestBody SystemSet systemSet) {
+        boolean flag = systemSetService.updateById(systemSet);
         if(flag) {
             return Result.ok();
         } else {
@@ -119,34 +115,26 @@ public class SystemSetController {
         }
     }
 
-    //7 批量删除医院设置
+    //7 批量删除系统设置
+    @ApiOperation(value = "批量删除系统设置")
     @DeleteMapping("batchRemove")
-    public Result batchRemoveHospitalSet(@RequestBody List<Long> idList) {
-        hospitalSetService.removeByIds(idList);
+    public Result batchRemoveSystemSet(@RequestBody List<Long> idList) {
+        systemSetService.removeByIds(idList);
         return Result.ok();
     }
 
-    //8 医院设置锁定和解锁
-    @PutMapping("lockHospitalSet/{id}/{status}")
-    public Result lockHospitalSet(@PathVariable Long id,
+    //8 系统设置锁定和解锁
+    @ApiOperation(value = "系统设置锁定和解锁")
+    @PutMapping("lockSystemSet/{id}/{status}")
+    public Result lockSystemSet(@PathVariable Long id,
                                   @PathVariable Integer status) {
         //根据id查询医院设置信息
-        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        SystemSet systemSet = systemSetService.getById(id);
         //设置状态
-        hospitalSet.setStatus(status);
+        systemSet.setStatus(status);
         //调用方法
-        hospitalSetService.updateById(hospitalSet);
+        systemSetService.updateById(systemSet);
         return Result.ok();
     }
 
-    //9 发送签名秘钥
-    @PutMapping("sendKey/{id}")
-    public Result lockHospitalSet(@PathVariable Long id) {
-        HospitalSet hospitalSet = hospitalSetService.getById(id);
-        String signKey = hospitalSet.getSignKey();
-        String hoscode = hospitalSet.getHoscode();
-        //TODO 发送短信
-        return Result.ok();
-    }
-    */
 }
